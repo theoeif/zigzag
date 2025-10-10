c78e86329a6660f173c92dd028201b87c145ae90
theoeiferman <teiferman27@gmail.com>
2025-10-10 17:39:15 +0200

    design icon Project

--- FRONTEND/zigzag/src/components/Header/Header.jsx
+++ FRONTEND/zigzag/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../index.css'
 import { useNavigate } from 'react-router-dom'; // Import useNavigate hook
 import { FaFilter } from 'react-icons/fa'; // Import FaFilter if needed
-import { CalendarToday } from '@mui/icons-material';
+import { CalendarToday, Folder } from '@mui/icons-material';
 
 const Header = ({ toggleLeftMenu, toggleFilterMenu }) => {
   const navigate = useNavigate(); // Initialize the useNavigate hook
@@ -15,11 +15,18 @@ const Header = ({ toggleLeftMenu, toggleFilterMenu }) => {
     navigate('/calendar'); // Navigate to calendar page
   };
 
+  const handleProjectsClick = () => {
+    navigate('/events'); // Navigate to projects page
+  };
+
   return (
     <div className="header">
       <button className="left-menu-icon" onClick={toggleLeftMenu}>☰</button>
       <h1 className="app-title" onClick={handleLogoClick}>zig zag</h1> {/* Add click handler here */}
       <div className="header-actions">
+        <button className="projects-icon" onClick={handleProjectsClick} title="Projets">
+          <Folder />
+        </button>
         <button className="calendar-icon" onClick={handleCalendarClick} title="Calendrier">
           <CalendarToday />
         </button>
