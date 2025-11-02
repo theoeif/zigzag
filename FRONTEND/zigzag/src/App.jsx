c1c086b3acd00d27ae4e84e4b6efd180c0bd0f81
theoeif <theo.eiferman@gmail.com>
2025-11-02 23:52:21 +0100

    deeplink with https + ios detection

--- FRONTEND/zigzag/src/App.jsx
+++ FRONTEND/zigzag/src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
       console.log('App: Cleaning up deep link listeners');
       removeDeepLinkListener();
     };
-  }, [navigate]);
+  }, []); // run only once on mount.
 
   return (
     <>
